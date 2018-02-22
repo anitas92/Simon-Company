1126461436a37fc4bd72fc084e11fd926738c77b
Anita Simon <anita.simon92@gmail.com>
2018-02-22 20:09:38 +0100

    js fade

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
-$(document).ready(function(){
+
     $("p").click(function(){
         $(this).hide();
        // $(this).fadeTo(5000, 1, function(){
-          $(this).fadeIn("2000",1);
+          $(this).fadeIn(2000);
     });
     
     //Esemeny kivaltasok
       $("p").click();
     
-});
+
