2f001f88a826adfffabaf918177cdefba13a8223
Anita Simon <anita.simon92@gmail.com>
2018-02-22 19:31:00 +0100

    basic js + pages

---
+++ main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+       // $(this).fadeTo(5000, 1, function(){
+          $(this).fadeIn("2000",1);
+    });
+    
+    //Esemeny kivaltasok
+      $("p").click();
+    
+});
