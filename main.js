7ebb91321c69fec90e5bf2714d6a620ceb40bd89
Anita Simon <anita.simon92@gmail.com>
2018-02-28 12:26:37 +0100

    gallery page photos - click function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
+ var imgLarge = false;
+
 $(document).ready(function(){
-    
     $("p").click(function(){
         $(this).hide();
        // $(this).fadeTo(5000, 1, function(){
@@ -11,3 +12,16 @@ $(document).ready(function(){
       $("p").click();
     
     });
+
+    
+   /* $("img").on("click", function(){
+        $(this).toggleClass("big-image");
+    });*/
+
+    $(".preview-holder img").on("click", function() {
+        var div = $(this).parent().clone();
+        div.addClass("big-image").on("click", function() {
+          $(this).remove();
+        });
+        $(document.body).append(div);
+      });
